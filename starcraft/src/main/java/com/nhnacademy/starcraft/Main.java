646961b9821116a0a1f7c39e67830540cb840ce5
f1v3-dev <f1v3@kakao.com>
2023-09-29 10:15:21 +0900

    Code: Exception handling

--- starcraft/src/main/java/com/nhnacademy/starcraft/Main.java
+++ starcraft/src/main/java/com/nhnacademy/starcraft/Main.java
@@ -5,6 +5,7 @@ import com.nhnacademy.starcraft.race.protoss.Protoss;
 import com.nhnacademy.starcraft.race.terran.Terran;
 import com.nhnacademy.starcraft.race.zerg.Zerg;
 import com.nhnacademy.starcraft.unit.Unit;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -19,22 +20,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("스타크래프트 게임을 시작합니다.");
-        System.out.println("플레이어의 종족을 선택하세요.");
-        System.out.println("1. 프로토스");
-        System.out.println("2. 테란");
-        System.out.println("3. 저그");
 
-        System.out.print("> ");
-        int number = sc.nextInt();
 
-        computerRace = selectRace((int) (Math.random() * 3) + 1);
-        playerRace = selectRace(number);
+        while (true) {
+            try {
+                System.out.println("플레이어의 종족을 선택하세요.");
+                System.out.println("1. 프로토스");
+                System.out.println("2. 테란");
+                System.out.println("3. 저그");
+                System.out.print("> ");
 
-        printUnits(computerRace);
-        System.out.println();
-
-        printUnits(playerRace);
-        System.out.println();
+                int number = sc.nextInt();
+                computerRace = selectRace((int) (Math.random() * 3) + 1);
+                playerRace = selectRace(number);
+                break;
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("숫자를 입력하세요.");
+            } catch (IllegalArgumentException e) {
+                sc.nextLine();
+                System.out.println(e.getMessage());
+            }
+        }
 
         System.out.println("게임을 시작합니다.");
         playGame(computerRace, playerRace, sc);
@@ -63,22 +70,34 @@ public class Main {
                 break;
             }
 
-            if (playerTurn) {
-                System.out.println("턴을 시작합니다.");
-                System.out.println("공격할 유닛의 번호와 공격받을 적군의 번호를 입력하세요.");
-                System.out.print("> ");
-                int attackUnitNumber = sc.nextInt();
-                int defenseUnitNumber = sc.nextInt();
-                attackUnit(attackUnitNumber, defenseUnitNumber, playerRace, computerRace);
-            } else {
-                System.out.println("컴퓨터의 턴입니다.");
-                int attackUnitNumber = random.nextInt(computerRace.getUnitCount()) + 1;
-                int defenseUnitNumber = random.nextInt(playerRace.getUnitCount()) + 1;
-                attackUnit(attackUnitNumber, defenseUnitNumber, computerRace, playerRace);
-            }
 
             printUnits(computerRace);
             printUnits(playerRace);
+
+            try {
+                if (playerTurn) {
+                    System.out.println("턴을 시작합니다.");
+                    System.out.println("공격할 유닛의 번호와 공격받을 적군의 번호를 입력하세요.(ex. 1 2)");
+                    System.out.println("");
+                    System.out.print("> ");
+                    int attackUnitNumber = sc.nextInt();
+                    int defenseUnitNumber = sc.nextInt();
+                    attackUnit(attackUnitNumber, defenseUnitNumber, playerRace, computerRace);
+                } else {
+                    System.out.println("컴퓨터의 턴입니다.");
+                    int attackUnitNumber = random.nextInt(computerRace.getUnitCount()) + 1;
+                    int defenseUnitNumber = random.nextInt(playerRace.getUnitCount()) + 1;
+                    attackUnit(attackUnitNumber, defenseUnitNumber, computerRace, playerRace);
+                }
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(e.getMessage());
+                continue;
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("숫자를 입력하세요.");
+                continue;
+            }
+
             playerTurn = !playerTurn;
             System.out.println();
         }
@@ -86,11 +105,19 @@ public class Main {
     }
 
     private static void attackUnit(int attackUnitNumber, int defenseUnitNumber, Race attackRace, Race defenseRace) {
+
+
         List<Unit> attackRaceUnits = attackRace.getUnitList();
         List<Unit> defenseRaceUnits = defenseRace.getUnitList();
 
-        Unit attackUnit = attackRaceUnits.get(attackUnitNumber - 1);
-        Unit defenseUnit = defenseRaceUnits.get(defenseUnitNumber - 1);
+        Unit attackUnit;
+        Unit defenseUnit;
+        try {
+            attackUnit = attackRaceUnits.get(attackUnitNumber - 1);
+            defenseUnit = defenseRaceUnits.get(defenseUnitNumber - 1);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("유닛 번호를 잘못 입력하셨습니다.");
+        }
 
         try {
             System.out.println(attackUnit.getName() + "이(가) " + defenseUnit.getName() + "을(를) 공격합니다.");
@@ -125,7 +152,7 @@ public class Main {
             case 3:
                 return new Zerg();
             default:
-                throw new IllegalArgumentException("잘못 입력하셨습니다.");
+                throw new IllegalArgumentException("1 ~ 3 사이의 값을 입력하세요.");
         }
     }
 }
