0a8826c724ca9fd7049f339977909d832b10b063
f1v3-dev <f1v3@kakao.com>
2023-10-11 19:43:29 +0900

    Code: 스프링 입문을 위한 자바 객체지향 원리와 이해

---
+++ Java/oop_in_java/src/Start6.java
@@ -0,0 +1,22 @@
+public class Start6 extends Thread {
+    static int share;
+
+    public static void main(String[] args) {
+        Start6 t1 = new Start6();
+        Start6 t2 = new Start6();
+
+        t1.start();
+        t2.start();
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(share++);
+
+            try {
+                sleep(2000);
+            } catch (InterruptedException ignore) {
+            }
+        }
+    }
+}
