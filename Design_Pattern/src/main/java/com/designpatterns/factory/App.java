05363d0e2e5624a6ba625438b47f742dff67a493
f1v3-dev <f1v3_dev@naver.com>
2024-04-02 15:59:11 +0900

    add: factory (ENUM)

--- Design_Pattern/src/main/java/com/designpatterns/factory/App.java
+++ Design_Pattern/src/main/java/com/designpatterns/factory/App.java
@@ -3,8 +3,8 @@ package com.designpatterns.factory;
 public class App {
 
     public static void main(String[] args) {
-        Coffee latte = CoffeeFactory.getCoffee("Latte", 4000);
-        Coffee americano = CoffeeFactory.getCoffee("Americano", 3000);
+        Coffee latte = CoffeeFactory.getCoffee(CoffeeType.Latte, 4000);
+        Coffee americano = CoffeeFactory.getCoffee(CoffeeType.Americano, 3000);
 
         System.out.println("Factory latte = " + latte);
         System.out.println("Factory americano = " + americano);
--- Design_Pattern/src/main/java/com/designpatterns/factory/CoffeeFactory.java
+++ Design_Pattern/src/main/java/com/designpatterns/factory/CoffeeFactory.java
@@ -2,13 +2,27 @@ package com.designpatterns.factory;
 
 public class CoffeeFactory {
 
-    public static Coffee getCoffee(String type, int price) {
-        if ("Latte".equalsIgnoreCase(type)) {
-            return new Latte(price);
-        } else if ("Americano".equalsIgnoreCase(type)) {
-            return new Americano(price);
-        } else {
-            return new DefaultCoffee();
+//    public static Coffee getCoffee(String type, int price) {
+//        if ("Latte".equalsIgnoreCase(type)) {
+//            return new Latte(price);
+//        } else if ("Americano".equalsIgnoreCase(type)) {
+//            return new Americano(price);
+//        } else {
+//            return new DefaultCoffee();
+//        }
+//    }
+
+    public static Coffee getCoffee(CoffeeType type, int price) {
+
+        switch (type) {
+            case Latte:
+                return new Latte(price);
+
+            case Americano:
+                return new Americano(price);
+
+            default:
+                return new DefaultCoffee();
         }
     }
 }
